80173324a28677609e3d010c79c50f44922f5b67
Eduardo <ecuenca4@gmail.com>
2024-08-29 13:37:38 -0500

    Cambios para chatbot

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    // Crear un nuevo script element
+    const script = document.createElement("script");
+    script.src = "//fw-cdn.com/10760529/3551031.js";
+    script.setAttribute("chat", "true");
+    script.setAttribute("widgetId", "4a3facd5-a37d-4f20-9fee-181a4b6732fa");
+
+    // Añadir el script al cuerpo del documento
+    document.body.appendChild(script);
+
+    // Limpiar el script al desmontar el componente
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []); // Este efecto se ejecuta solo una vez cuando el componente se monta
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenido a mi página</h1>
+        {/* Aquí puedes añadir más contenido */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
